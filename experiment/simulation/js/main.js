c5f3b62575bcda16ea4395895905ef6b5bfad82a
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-05-12 20:39:12 +0530

    Added exp1 graphics and animation.

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -10,16 +10,204 @@ document.addEventListener('DOMContentLoaded', function(){
 	playButton.addEventListener('click', function() { window.clearTimeout(tmHandle); tmHandle = setTimeout(draw, 1000 / fps); });
 	restartButton.addEventListener('click', function() {restart();});
 
+	class container {
+		constructor(height, width, radius, x, y) {
+			this.height = height;
+			this.width = width;
+			this.radius = radius;
+			this.pos = [x, y];
+		};
+
+		draw(ctx) {
+			ctx.fillStyle = "white";
+			ctx.lineWidth = 3;
+
+			if (this.width < 2 * this.radius) 
+			{
+				this.radius = this.width / 2;
+			}
+
+			if (this.height < 2 * this.radius) 
+			{
+				this.radius = this.height / 2;
+			}
+
+			ctx.beginPath();
+			ctx.moveTo(this.pos[0] + this.radius, this.pos[1]);
+			ctx.arcTo(this.pos[0] + this.width, this.pos[1], this.pos[0] + this.width, this.pos[1] + this.height, this.radius);
+			ctx.arcTo(this.pos[0] + this.width, this.pos[1] + this.height, this.pos[0], this.pos[1] + this.height, this.radius);
+			ctx.arcTo(this.pos[0], this.pos[1] + this.height, this.pos[0], this.pos[1], this.radius);
+			ctx.arcTo(this.pos[0], this.pos[1], this.pos[0] + this.width, this.pos[1], this.radius);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			const e1 = [this.pos[0] + this.width, this.pos[1]], e2 = [...this.pos];
+			const gradX = (e1[0] - e2[0]) / -4, gradY = 10;
+
+			ctx.beginPath();
+			ctx.moveTo(e2[0], e2[1]);
+			curvedArea(ctx, e2, -1 * gradX, -1 * gradY);
+			curvedArea(ctx, e1, gradX, gradY);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		};
+
+		updatePos(translate, lim, step) {
+			if(step === 2)
+			{
+				return;
+			}
+
+			this.pos[0] += translate[0];
+			this.pos[1] += translate[1];
+
+			if(this.pos[0] === lim[0])
+			{
+				translate[0] = 0;
+			}
+
+			if(this.pos[1] === lim[1])
+			{
+				translate[1] = 0;
+			}
+		}
+	};
+
+	class soil {
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+		};
+
+		draw(ctx) {
+			ctx.fillStyle = "#654321";
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.rect(this.pos[0], this.pos[1], this.width, this.height)
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		};
+
+		updatePos(translate, lim, step) {
+			if(step === 1)
+			{
+				return;
+			}
+
+			this.pos[0] += translate[0];
+			this.pos[1] += translate[1];
+
+			if(this.pos[0] === lim[0])
+			{
+				translate[0] = 0;
+			}
+
+			if(this.pos[1] === lim[1])
+			{
+				translate[1] = 0;
+			}
+		}
+	};
+
+	class weight{
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+			this.img = new Image();
+			this.img.src = '../images/weighing machine.png';
+			this.img.onload = () => {ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);}; 
+		};
+
+		draw(ctx) {
+			ctx.drawImage(objs['weight'].img, objs['weight'].pos[0], objs['weight'].pos[1], objs['weight'].width, objs['weight'].height);
+		}
+	};
+
+	class oven {
+		constructor(height, width, x, y) {
+			this.height = height;
+			this.width = width;
+			this.pos = [x, y];
+		};
+
+		draw(ctx) {
+			ctx.fillStyle = "white";
+			ctx.lineWidth = 3;
+			ctx.beginPath();
+			ctx.moveTo(this.pos[0], this.pos[1]);
+			ctx.lineTo(this.pos[0] + this.width, this.pos[1]);
+			ctx.lineTo(this.pos[0] + this.width, this.pos[1] + this.height);
+			ctx.lineTo(this.pos[0], this.pos[1] + this.height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			const divide = 0.80;
+			ctx.fillStyle = "red";
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.moveTo(this.pos[0], this.pos[1] + divide * this.height);
+			ctx.lineTo(this.pos[0] + this.width, this.pos[1] + divide * this.height);
+			ctx.lineTo(this.pos[0] + this.width, this.pos[1] + this.height);
+			ctx.lineTo(this.pos[0], this.pos[1] + this.height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			const gap = 0.05;
+			ctx.fillStyle = "pink";
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.moveTo(this.pos[0] + gap * this.width, this.pos[1] + gap * this.height);
+			ctx.lineTo(this.pos[0] + (1 - gap) * this.width, this.pos[1] + gap * this.height);
+			ctx.lineTo(this.pos[0] + (1 - gap) * this.width, this.pos[1] + divide * this.height);
+			ctx.lineTo(this.pos[0] + gap * this.width, this.pos[1] + divide * this.height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			const margin = [0.30, 0.05];
+			ctx.fillStyle = "white";
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.moveTo(this.pos[0] + margin[0] * this.width, this.pos[1] + (margin[1] + divide) * this.height);
+			ctx.lineTo(this.pos[0] + (1 - margin[0]) * this.width, this.pos[1] + (margin[1] + divide) * this.height);
+			ctx.lineTo(this.pos[0] + (1 - margin[0]) * this.width, this.pos[1] + (1 - margin[1]) * this.height);
+			ctx.lineTo(this.pos[0] + margin[0] * this.width, this.pos[1] + (1 - margin[1]) * this.height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			const buttonGapX = 0.10;
+			ctx.fillStyle = "white";
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.moveTo(this.pos[0] + buttonGapX * this.width, this.pos[1] + (margin[1] + divide) * this.height);
+			ctx.lineTo(this.pos[0] + (margin[0] - buttonGapX) * this.width, this.pos[1] + (margin[1] + divide) * this.height);
+			ctx.lineTo(this.pos[0] + (margin[0] - buttonGapX) * this.width, this.pos[1] + (1 - margin[1]) * this.height);
+			ctx.lineTo(this.pos[0] + buttonGapX * this.width, this.pos[1] + (1 - margin[1]) * this.height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		};
+	};
+
 	function init()
 	{
-	}
+		step = 0;
+	};
 
 	function restart() 
 	{ 
 		window.clearTimeout(tmHandle); 
 		init();
 		tmHandle = window.setTimeout(draw, 1000 / fps); 
-	}
+	};
 
 	const sliders = ["slider"];
 	sliders.forEach(function(elem, ind) {
@@ -32,6 +220,12 @@ document.addEventListener('DOMContentLoaded', function(){
 			restart();
 		};
 	});
+
+	function curvedArea(ctx, e, gradX, gradY)
+	{
+		ctx.bezierCurveTo(e[0], e[1] += gradY, e[0] += gradX, e[1] += gradY, e[0] += gradX, e[1]);
+		ctx.bezierCurveTo(e[0] += gradX, e[1], e[0] += gradX, e[1] -= gradY, e[0], e[1] -= gradY);
+	};
 
 	function drawGraph(Xaxis, Yaxis, id, Xlabel, color) {
 		try {
@@ -80,53 +274,86 @@ document.addEventListener('DOMContentLoaded', function(){
 			console.error(err);
 			alert(err);
 		}
-	}
+	};
 
 	const canvas = document.getElementById("main");
-	canvas.width = 1200;
+	canvas.width = 840;
 	canvas.height = 600;
 	canvas.style = "border:3px solid";
 	const ctx = canvas.getContext("2d");
 
 	const fill = "#A9A9A9", border = "black", lineWidth = 1.5, fps = 15;
+	const objs = {
+		"weight": new weight(270, 240, 90, 300),
+		"oven": new oven(330, 240, 520, 240),
+		"container": new container(120, 150, 8, 600, 30),
+		"soil": new soil(90, 150, 90, 30),
+	}, keys = Object.keys(objs);
+
+	canvas.addEventListener('click', function(event) { 
+		const canvasPos = [event.pageX - canvas.offsetLeft, (20 / 17) * (event.pageY - canvas.offsetTop)];
+
+		keys.forEach(function(val, ind){
+			if(canvasPos[0] >= objs[val].pos[0] && canvasPos[0] <= objs[val].pos[0] + objs[val].width && canvasPos[1] >= objs[val].pos[1] && canvasPos[1] <= objs[val].pos[1] + objs[val].height)
+			{
+				if(step === 0 && val === "container")
+				{
+					translate[0] = -5;
+					translate[1] = 5;
+					lim[0] = 135;
+					lim[1] = 250;
+					step = (step + 1) % 4;
+				}
+
+				else if(step === 1 && val === "soil")
+				{
+					translate[0] = 5;
+					translate[1] = 5;
+					lim[0] = 135;
+					lim[1] = 280;
+					step = (step + 1) % 4;
+				}
+
+				else if(step === 2 && val === "container")
+				{
+					translate[0] = 5;
+					translate[1] = 5;
+					lim[0] = 560;
+					lim[1] = 400;
+					step = (step + 1) % 4;
+				}
+
+				else if(step === 3 && val === "container")
+				{
+					translate[0] = -5;
+					translate[1] = -5;
+					lim[0] = 135;
+					lim[1] = 250;
+					step = (step + 1) % 4;
+				}
+			}
+		});
+	});
 
 	// Input Parameters 
+	let step, translate = [0, 0], lim = [-1, -1];
 	init();
-
-	//function drawGround(ctx, ground)
-	//{
-		//ctx.save();
-		//ctx.fillStyle = "pink";
-		//ctx.beginPath();
-		//ctx.moveTo(ground[0][0], ground[0][1]);
-
-		//ground.forEach(function(g, index){
-			//const next = (index + 1) % ground.length;
-			//ctx.lineTo(ground[next][0], ground[next][1]);
-		//});
-		//ctx.closePath();
-		//ctx.fill();
-		//ctx.stroke();
-		//ctx.restore();
-	//}
 
 	function draw()
 	{
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.fillStyle = fill;
-		ctx.lineWidth = lineWidth;
+		ctx.clearRect(0, 0, canvas.width, canvas.height); 
 		ctx.lineCap = "round";
 		ctx.lineJoin = "round";
 
-		//drawGround(ctx, ground);
+		keys.forEach(function(val, ind){
+			objs[val].draw(ctx);
+		});
 
-		ctx.beginPath();
-		ctx.closePath();
-		ctx.fill();
-		ctx.stroke();
+		objs['container'].updatePos(translate, lim, step);
+		objs['soil'].updatePos(translate, lim, step);
 
 		tmHandle = window.setTimeout(draw, 1000 / fps);
-	}
+	};
 
 	let tmHandle = window.setTimeout(draw, 1000 / fps);
-})
+});
